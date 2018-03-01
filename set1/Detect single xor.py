497c1046f6453c6dd3837ae7ec43c4600cb1fba8
root <root@localhost.localdomain>
2018-03-01 10:12:32 +0700

    up ch4

---
+++ set1/Detect single xor.py
@@ -0,0 +1,63 @@
+def xor(a1, a2):
+    a = bytearray(len(a1))
+    for i in range(len(a1)):
+        a[i] = a1[i] ^ a2[i]
+    return a
+
+
+def score(s):
+    freq = {}
+    freq[' '] = 700000000
+    freq['e'] = 390395169
+    freq['t'] = 282039486
+    freq['a'] = 248362256
+    freq['o'] = 235661502
+    freq['i'] = 214822972
+    freq['n'] = 214319386
+    freq['s'] = 196844692
+    freq['h'] = 193607737
+    freq['r'] = 184990759
+    freq['d'] = 134044565
+    freq['l'] = 125951672
+    freq['u'] = 88219598
+    freq['c'] = 79962026
+    freq['m'] = 79502870
+    freq['f'] = 72967175
+    freq['w'] = 69069021
+    freq['g'] = 61549736
+    freq['y'] = 59010696
+    freq['p'] = 55746578
+    freq['b'] = 47673928
+    freq['v'] = 30476191
+    freq['k'] = 22969448
+    freq['x'] = 5574077
+    freq['j'] = 4507165
+    freq['q'] = 3649838
+    freq['z'] = 2456495
+    
+    score = 0
+    for c in s.lower():
+        if c in freq:
+            score += freq[c]
+    return score
+
+max_score = None
+english_plaintext = None
+temp_score = None
+
+for line in open("ch4.txt", "r"):
+    line =line.rstrip()
+
+    a1 = bytearray.fromhex(line)
+
+    for i in xrange(256):
+        a2 = [i] * len(a1)
+        plaintext = bytes(xor(a1,a2))
+        temp_score = score(plaintext)
+
+        if temp_score > max_score or not max_score:
+            max_score = temp_score
+            english_plaintext = plaintext
+            key = chr(i)
+
+print key, english_plaintext
