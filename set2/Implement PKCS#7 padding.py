fef4acf373a4ca177149b3c592bbe2f4819a33c6
hatakepham <son09121995@gmail.com>
2018-03-05 04:56:17 -0500

    update ch9

--- set2/Implement PKCS#7 padding.py
+++ set2/Implement PKCS#7 padding.py
@@ -12,9 +12,19 @@ def pad_pksc7(buffer, block_size):
 	#add padding
 	return new_buffer
 
+def  unpad_pkcs7(buffer):
+	padding = buffer[-1]
+	for i in range(len(buffer)- 1, len(buffer) - padding, -1):
+		if buffer[i] != buffer[-1]:
+			return buffer
+	new_buffer = bytearray()
+	new_buffer[:] = buffer[:-padding]
+	return new_buffer
+	#ham unpad
+
 buffer = bytearray("YELLOW SUBMARINE")
 a = pad_pksc7(buffer, 20)
 
 hex = "".join("%02x" % b for b in a)
-print len(hex)
 print hex
+print unpad_pkcs7(a)
